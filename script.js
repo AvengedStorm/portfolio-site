a18184e2d8376dfa2003271f9f253fed649269bb
omri <DJCreeperNo1@gmail.com>
2021-04-03 23:40:08 +0300

    adding JS interactivity and changing some html ids

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-let aboutDisplay = () => {
-    document.getElementById('About').style.display = "shown";
-    document.getElementById('About').style.display = "hidden";
+let homeDisplay = () => {
+    document.getElementById('About-Me').style.display = 'initial';
+    document.getElementById('Projects').style.display = 'initial';
+    document.getElementById('Contact').style.display = 'initial';
 }
 
-Element.addEventListener.getElementById('about')("click", aboutDisplay);
+Element.addEventListener.getElementById('home')("click", homeDisplay);
+
+let aboutMeDisplay = () => {
+    document.getElementById('About-Me').style.display = 'initial';
+    document.getElementById('Projects').style.display = 'none';
+    document.getElementById('Contact').style.display = 'none';
+}
+
+Element.addEventListener.getElementById('About-Me')("click", aboutMeDisplay);
+
+let projectsDisplay = () => {
+    document.getElementById('About-Me').style.display = 'none';
+    document.getElementById('Projects').style.display = 'initial';
+    document.getElementById('Contact').style.display = 'none';
+}
+
+Element.addEventListener.getElementById('Projects')("click", projectsDisplay);
+
+let contactDisplay = () => {
+    document.getElementById('About-Me').style.display = 'none';
+    document.getElementById('Projects').style.display = 'none';
+    document.getElementById('Contact').style.display = 'initial';
+}
+
+Element.addEventListener.getElementById('Contact')("click", contactDisplay);
