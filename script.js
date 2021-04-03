3bbaebc66ec651b9aa39da735102f2fa313c282d
omri <DJCreeperNo1@gmail.com>
2021-04-03 21:19:36 +0300

    starting interactivity

---
+++ script.js
@@ -0,0 +1,6 @@
+let aboutDisplay = () => {
+    document.getElementById('About').style.display = "shown";
+    document.getElementById('About').style.display = "hidden";
+}
+
+Element.addEventListener.getElementById('about')("click", aboutDisplay);
