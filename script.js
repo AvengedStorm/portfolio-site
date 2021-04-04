58866137a8bb3072a5996ea075f2c1227562d7a6
omri <DJCreeperNo1@gmail.com>
2021-04-04 05:43:12 +0300

    changing alignment, styling and JS code

--- script.js
+++ script.js
@@ -1,50 +1,75 @@
-let aboutmedisplay = document.getElementById('About-Me');
-let projectdisplay = document.getElementById('Projects');
-let contactdisplay = document.getElementById('Contact');
-let selfiedisplay = document.getElementById('Selfie');
-let homedisplay = document.getElementById('home');
+const aboutmedisplay = document.getElementById('About');
+const projectdisplay = document.getElementById('Projects');
+const contactdisplay = document.getElementById('Contact');
+const selfiedisplay = document.getElementById('Selfie');
 
-let homeDisplay = () => {
-    aboutmedisplay.style.display = 'initial';
-    projectdisplay.style.display = 'initial';
-    contactdisplay.style.display = 'initial';
-    selfiedisplay.style.display = 'initial';
-}
+document.getElementsByName("selfie").style.display = 'none';
 
-homedisplay.addEventListener("click", homeDisplay);
 
-let aboutMeDisplay = () => {
-    aboutmedisplay.style.display = 'initial';
-    projectdisplay.style.display = 'none';
-    contactdisplay.style.display = 'none';
-    selfiedisplay.style.display = 'none';
-}
+// aboutmedisplay.style.display = "initial";;
+// projectdisplay.style.display = "initial";;
+// contactdisplay.style.display = "initial";;
+// selfiedisplay.style.display = "initial";;
 
-aboutmedisplay.addEventListener("click", aboutMeDisplay);
+// projectdisplay.onclick = function() {
+//     projectdisplay.style.display = "initial";
+//     contactdisplay.style.display = "none";
+//     selfiedisplay.style.display = "none";
+//     aboutmedisplay.style.display = "none";
+// }
 
-let projectsDisplay = () => {
-    aboutmedisplay.style.display = 'none';
-    projectdisplay.style.display = 'initial';
-    contactdisplay.style.display = 'none';
-    selfiedisplay.style.display = 'none';
-}
+// contactdisplay.onclick = function() {
+//     projectdisplay.style.display = "none";
+//     contactdisplay.style.display = "initial";
+//     selfiedisplay.style.display = "none";
+//     aboutmedisplay.style.display = "none";
+// }
 
-projectdisplay.addEventListener("click", projectsDisplay);
+// selfiedisplay.onclick = function() {
+//     projectdisplay.style.display = "none";
+//     contactdisplay.style.display = "none";
+//     selfiedisplay.style.display = "initial";
+//     aboutmedisplay.style.display = "none";
+// }
 
-let contactDisplay = () => {
-    aboutmedisplay.style.display = 'none';
-    projectdisplay.style.display = 'none';
-    contactdisplay.style.display = 'initial';
-    selfiedisplay.style.display = 'none';
-}
 
-contactdisplay.addEventListener("click", contactDisplay);
 
-let selfieDisplay = () => {
-    aboutmedisplay.style.display = 'none';
-    projectdisplay.style.display = 'none';
-    contactdisplay.style.display = 'none';
-    selfiedisplay.style.display = 'initial';
-}
 
-selfiedisplay.addEventListener("click", selfieDisplay);
+
+
+
+// const aboutMeDisplay = () => {
+//     aboutmedisplay.style.display = 'initial';
+//     projectdisplay.style.display = 'none';
+//     contactdisplay.style.display = 'none';
+//     selfiedisplay.style.display = 'none';
+// }
+
+// aboutmedisplay.addEventListener("click", aboutMeDisplay);
+
+// const projectsDisplay = () => {
+//     aboutmedisplay.style.display = 'none';
+//     projectdisplay.style.display = 'initial';
+//     contactdisplay.style.display = 'none';
+//     selfiedisplay.style.display = 'none';
+// }
+
+// projectdisplay.addEventListener("click", projectsDisplay);
+
+// const contactDisplay = () => {
+//     aboutmedisplay.style.display = 'none';
+//     projectdisplay.style.display = 'none';
+//     contactdisplay.style.display = 'initial';
+//     selfiedisplay.style.display = 'none';
+// }
+
+// contactdisplay.addEventListener("click", contactDisplay);
+
+// const selfieDisplay = () => {
+//     aboutmedisplay.style.display = 'none';
+//     projectdisplay.style.display = 'none';
+//     contactdisplay.style.display = 'none';
+//     selfiedisplay.style.display = 'initial';
+// }
+
+// selfiedisplay.addEventListener("click", selfieDisplay);
